7d977930db1b25c70b1203eb1fe23c1ef99f8d29
Satria L <sakanario@gmail.com>
2024-10-25 11:00:04 +0700

    add fitur print nppkb

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import BaseNppkb from "./component/BaseNppkb";
+import { useRef } from "react";
+import { useReactToPrint } from "react-to-print";
 
 function App() {
+  const baseNppkbRef = useRef(null);
+  const handlePrintBaseNppkbRef = useReactToPrint({ contentRef: baseNppkbRef });
+
   return (
-    <>
-      <BaseNppkb />
-    </>
+    <div>
+      <div
+        style={{ height: "100vh", gap: "8px" }}
+        className="d-flex align-items-center justify-content-center"
+      >
+        <button className="btn btn-primary" onClick={handlePrintBaseNppkbRef}>
+          Print NPPKB
+        </button>
+        <button className="btn btn-primary" onClick={handlePrintBaseNppkbRef}>
+          Print Info PKB
+        </button>
+      </div>
+
+      <div className="d-none">
+        <BaseNppkb ref={baseNppkbRef} />
+      </div>
+    </div>
   );
 }
 
